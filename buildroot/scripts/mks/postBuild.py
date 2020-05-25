50dbf515787fe2acba2011b37d5784948878869e
Delwin Best <mail@delwinbest.com>
2020-05-25 11:05:04 +0900

    rebuilt.

--- buildroot/scripts/mks/postBuild.py
+++ buildroot/scripts/mks/postBuild.py
@@ -4,14 +4,22 @@ from distutils.dir_util import copy_tree, remove_tree, mkpath
 import shutil 
 
 def after_build(source, target, env):
-    # Remove any existing Folder
     dst_dir = env['PROJECT_DIR'] + "/Copy to SD Card - MKS/"
+    # Backup Config File
+    dst_filename = env['PROJECT_BUILD_DIR'] + "/" + env['PIOENV'] + "/config.ini"
+    source_filename = dst_dir + "config.ini"
+    shutil.copyfile(source_filename, dst_filename)
+    # Remove any existing Folder
     if os.path.exists(dst_dir):
         remove_tree(dst_dir)
         mkpath(dst_dir)
     # Copy Bin File to SD Card Folder
     source_filename = env['PROJECT_BUILD_DIR'] + "/" + env['PIOENV'] + "/" + env['PROGNAME'] + ".bin"
     dst_filename = dst_dir + env['PROGNAME'] + ".bin"
+    shutil.copyfile(source_filename, dst_filename)
+     # Restore Config file
+    source_filename = env['PROJECT_BUILD_DIR'] + "/" + env['PIOENV'] + "/config.ini"
+    dst_filename = dst_dir + "config.ini"
     shutil.copyfile(source_filename, dst_filename)
     # Copy BMP folder to SD Card Folder
     src_dir = env['PROJECT_DIR'] + "/Copy to SD Card root directory to update - Unified Menu Material theme/TFT28"
